51e91f5621e992c45828d29505123083b50498c6
Muhammad ishaq <muhammadishaqchd622@gmail.com>
2026-02-25 09:32:55 -0800

    resolve import issue

--- src/components/homeComponents/Footer.jsx
+++ src/components/homeComponents/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Phone, Mail, Facebook, Twitter, Linkedin, Instagram } from "lucide-react";
-import Logo from "../../assets/logo.png";
+import Logo from "../../assets/Logo.png";
 
 const Footer = () => {
   return (
